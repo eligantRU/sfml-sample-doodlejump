2b3acdd45a465ff3ad196bbe53badac483276978
eligantRU <eligant.ru@gmail.com>
2017-01-06 14:16:57 +0400

    Реализовал класс плит

--- DoodleJump_2/sheet.h
+++ DoodleJump_2/sheet.h
@@ -8,9 +8,12 @@ static const unsigned WINDOW_FRAME_LIMIT = 60;
 static const sf::Vector2f DOODLE_SIZE = { 50, 80 };
 static const sf::Vector2f DOODLE_INITIAL_POSITION = 0.5f * (sf::Vector2f(WINDOW_SIZE) - DOODLE_SIZE);
 
+static const sf::Vector2f PLATE_SIZE = { 80, 20 };
+
 static const auto STEP = 6.f;
 
 static const sf::Color GREEN = { 0, 127, 0 };
+static const sf::Color BROWN = { 65, 25, 0 };
 static const sf::Color WHITE = { 255, 255, 255 };
 
 enum class DirectionX
