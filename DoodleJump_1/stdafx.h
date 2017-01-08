51457dade52a2491f81d8cbd7f6c4e7e53a7f80d
eligantRU <eligant.ru@gmail.com>
2017-01-08 18:00:52 +0400

    Прибрался в stdafx.h

--- DoodleJump_1/stdafx.h
+++ DoodleJump_1/stdafx.h
@@ -2,13 +2,8 @@
 
 #include "targetver.h"
 
-#include <iostream>
-
 #pragma warning (push, 3)
 
 #include <SFML/Graphics.hpp>
-//#include <SFML/Audio.hpp>
-//#include <SFML/OpenGL.hpp>
-//#include <SFML/Network.hpp>
 
 #pragma warning (pop)
--- DoodleJump_2/stdafx.h
+++ DoodleJump_2/stdafx.h
@@ -2,13 +2,8 @@
 
 #include "targetver.h"
 
-#include <algorithm>
-
 #pragma warning (push, 3)
 
 #include <SFML/Graphics.hpp>
-//#include <SFML/Audio.hpp>
-//#include <SFML/OpenGL.hpp>
-//#include <SFML/Network.hpp>
 
 #pragma warning (pop)
